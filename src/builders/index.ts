faebaf6add1561e2456891e678dfc1922ba76e32
Bishwendu Kundu <bishwenduk029@gmail.com>
2021-04-13 15:24:37 +0530

    change type errors

--- src/builders/index.ts
+++ src/builders/index.ts
@@ -1,9 +1,7 @@
 import type { Logger } from 'pino'
-import { watch } from 'chokidar'
 import { Service } from 'esbuild'
 import fs from 'fs'
 import Path from 'path'
-import { ThrottledDelayer } from 'ts-primitives'
 
 export interface CommonBuilderOptions {
   logger: Logger
@@ -12,28 +10,13 @@ export interface CommonBuilderOptions {
 }
 
 export abstract class Builder {
-  private readonly delayer: ThrottledDelayer<unknown>
   abstract readonly logger: Logger
   abstract readonly service: Service
   abstract readonly rootDir: string
   abstract readonly pageSourcePath: string
   abstract readonly pageBuildPath: string
-  private readonly watcher = watch([], {
-    ignoreInitial: true,
-    usePolling: true,
-    interval: 16,
-  })
 
-  public abstract build(): Promise<Void>
-
-  private async start() {
-    await this.delayer.trigger(() => {
-      return this.build()
-    })
-    this.watcher.on('all', () => {
-      this.build()
-    })
-  }
+  public abstract build(): Promise<any>
 
   async setup() {
     const modulePaths = await fs.promises.readdir(
